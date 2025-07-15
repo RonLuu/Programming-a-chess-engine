61da2d056b8c53a192a59ad79d3515306abab9a4
trieukhail <trieukhail@student.unimelb.edu.au>
2025-07-15 21:41:28 +1000

    Add popBit function

--- bitboard.c
+++ bitboard.c
@@ -1,14 +1,20 @@
 #include "stdio.h"
 #include "defs.h"
 
+int popBit(U64 *bb)
+{
+    U64 b = *bb ^ (*bb - 1);
+    unsigned int fold = (unsigned) ((b & 0xffffffff) ^ (b>>32));
+    *bb &= *bb - 1;
+    return BitTable[(fold * 0x783a9b23) >> 26];
+}
+
 int countBit(U64 bitNum)
 {
     int totalBit = 0;
     for (totalBit++; bitNum; bitNum &= bitNum-1);
     return totalBit;
 }
-
-
 
 void printBitBoard(U64 bitboard)
 {
